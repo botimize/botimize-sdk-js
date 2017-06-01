0033700d3f687451d123e70159ee791614c353db
ggm <godgunman@gmail.com>
2017-06-01 18:55:22 +0800

    Support telegram and line platform

--- src/botimize.js
+++ src/botimize.js
@@ -1,6 +1,8 @@
 import request from 'request';
+import _ from 'lodash';
 
 const API_URL = 'https://api.botimize.io';
+const SUPPORTED_PLATFORM = ['facebook', 'telegram', 'line', 'generic'];
 
 function makeRequest(options, cb) {
   // send request to api server
@@ -25,11 +27,17 @@ class BotimizeCore {
    *
    *  @param apiKey the access token for sending events to botimize api server.
    */
-  constructor(apiKey, platform, options) {
+  constructor(apiKey, platform, options = {}) {
     if (!apiKey) {
       throw new Error('No API key provided');
     }
-    if (platform !== 'facebook') {
+    if (!platform) {
+      throw new Error('platform is not defined');
+    }
+
+    platform = platform.toLocaleLowerCase();
+
+    if (SUPPORTED_PLATFORM.indexOf(platform) === -1) {
       throw new Error('Specified platform is not supported: ' + platform);
     }
     this.apiKey = apiKey;
@@ -67,7 +75,7 @@ class BotimizeCore {
         raw: _props,
       },
     };
-    makeRequest(options, (error) => {
+    makeRequest(options, error => {
       if (error) {
         console.log('failed to send track event to botimize server.');
         console.log(error);
@@ -85,18 +93,37 @@ class BotimizeCore {
 
   logOutgoing(data, source = 'npm') {
     const prefix = `[botimize][${this.platform}][outgoing][${source}]`;
-    if (this.platform === 'facebook' && source === 'npm') {
-      let newData = data.json;
-      newData.access_token = data.qs.access_token;
-      if (this.debug) {
-        console.log(`${prefix}: ${JSON.stringify(newData, null, 2)}`);
-      }
-      this.track('outgoing', newData);
-    } else {
-      if (this.debug) {
-        console.log(`${prefix}: ${JSON.stringify(data, null, 2)}`);
-      }
-      this.track('outgoing', data);
+    let formatedData = {};
+    if (_.isEmpty(data.json) === false) {
+      console.warn('deprecated usage');
+    }
+
+    if (_.isEmpty(data.body) && _.isEmpty(data.json)) {
+      console.error('empty body');
+    }
+
+    if (this.platform === 'generic') {
+      formatedData = data;
+    } else if (this.platform === 'facebook' && source === 'npm') {
+      formatedData = data.body || data.json;
+      formatedData.accessToken = data.qs.access_token || data.access_token;
+    } else if (this.platform === 'telegram' && source === 'npm') {
+      const reg = /https:\/\/api\.telegram\.org\/bot(.*)\/(.*)/g;
+      const matches = reg.exec(data.uri || data.url);
+      formatedData = data.body || data.json;
+      formatedData.token = matches[1];
+      formatedData.method = matches[2];
+    } else if (this.platform === 'line' && source === 'npm') {
+      formatedData = data.body || data.json;
+      formatedData.channelAccessToken = data.headers['Authorization']
+        .replace('Bearer', '')
+        .trim();
+    }
+
+    this.track('outgoing', formatedData);
+
+    if (this.debug) {
+      console.log(`${prefix}: ${JSON.stringify(formatedData, null, 2)}`);
     }
   }
 
@@ -115,7 +142,7 @@ class BotimizeCore {
       json: true,
       body: data,
     };
-    makeRequest(options, (error) => {
+    makeRequest(options, error => {
       if (error) {
         console.log('failed to send notification to botimize server.');
         console.log(error);
